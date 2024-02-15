243a52604f5220c7b448ea9336b86e9d3532d9c0
Milton <juninhoeufrasio2017@hotmail.com>
2024-02-15 17:10:35 -0300

    atualizando

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client'; // Importando createRoot de 'react-dom/client' em vez de 'react-dom'
 import './index.css';
-import App from './App';
+import Calculator from './main/calculator';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <div>
+    <h1>Calculadora</h1>
+    <Calculator />
+  </div>
+);    
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
